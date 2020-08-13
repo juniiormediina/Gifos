6e260ce3cffb3996c7be3257ee4eadd06f099e31
juniiormediina <juniiormediina@gmail.com>
2020-08-13 11:29:39 -0500

    Se inicia con las vistas de las funcionalidades a recibir

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 document.getElementById('btn-menu').addEventListener('click', () => {
         var iconMenu = document.getElementById('icon');
         let fullMenu = document.getElementsByClassName('full-menu')[0];
-        if (iconMenu.classList.contains('class')) {
+        if (iconMenu.classList.contains('fa-times')) {
                 iconMenu.classList.toggle('fa-times');
                 iconMenu.classList.toggle('fa-bars');
-                fullMenu.style.left = '0%';
+                fullMenu.style.left = '-100%';
         } else{
                 iconMenu.classList.toggle('fa-bars');
                 iconMenu.classList.toggle('fa-times');
-                fullMenu.style.left = '-100%';
+                fullMenu.style.left = '0%';
         }
 });
+
